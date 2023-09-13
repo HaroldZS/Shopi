9b8295bbdc641e150f512b26c0bf1284cfe3276e
haroldZS <haroldzs1024@gmail.com>
2023-09-13 17:34:27 -0400

    Aside detail

---
+++ src/Components/productDetail/index.jsx
@@ -0,0 +1,12 @@
+const ProductDetail = () => {
+  return (
+    <aside className="flex flex-col fixed bg-white right-0 border top-[68px] border-black rounded-lg w-[360px] h-[calc(100vh-68px)]">
+      <div className="flex justify-between items-center p-6">
+        <h2 className="font-medium text-xl">Detail</h2>
+        <div>x</div>
+      </div>
+    </aside>
+  );
+};
+
+export default ProductDetail;
