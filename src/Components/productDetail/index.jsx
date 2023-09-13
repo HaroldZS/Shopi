75cd94e803d67eed54dfdb05bae983ad849bee7d
haroldZS <haroldzs1024@gmail.com>
2023-09-13 17:49:50 -0400

    Heroicons icons

--- src/Components/productDetail/index.jsx
+++ src/Components/productDetail/index.jsx
@@ -1,9 +1,11 @@
+import { XMarkIcon } from "@heroicons/react/24/solid";
+
 const ProductDetail = () => {
   return (
     <aside className="flex flex-col fixed bg-white right-0 border top-[68px] border-black rounded-lg w-[360px] h-[calc(100vh-68px)]">
       <div className="flex justify-between items-center p-6">
         <h2 className="font-medium text-xl">Detail</h2>
-        <div>x</div>
+        <div><XMarkIcon className="h-6 w-6 text-black"></XMarkIcon></div>
       </div>
     </aside>
   );
