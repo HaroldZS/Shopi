1d0e766d57780e76fd59661f44733829ab8e66dd
haroldZS <haroldzs1024@gmail.com>
2023-09-16 19:07:38 -0400

    Sign in page handle buttons

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,22 +1,39 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 import Layout from "../../Components/Layout";
-import { initializeLocalStorage } from "../../Context";
+import { ShopiCartContext, initializeLocalStorage } from "../../Context";
 
 function SignIn() {
+  const { account } = useContext(ShopiCartContext);
+
+  const localStorageAccount = localStorage.getItem("account");
+  const parsedAccount = JSON.parse(localStorageAccount);
+
+  const noAccountInLocalStorage = parsedAccount
+    ? Object.keys(parsedAccount).length === 0
+    : true;
+  const noAccountInLocalState = account
+    ? Object.keys(account).length === 0
+    : true;
+  const hasUserAnAccount = !noAccountInLocalStorage || !noAccountInLocalState;
+
   return (
     <Layout>
       <h1 className="font-medium text-xl text-center mb-6 w-80">Welcome!</h1>
       <div className="flex flex-col w-80">
         <p>
           <span className="font-ligth text-sm">Email: </span>
-          <span>haroldzs@gmail.com</span>
+          <span>{parsedAccount?.email}</span>
         </p>
         <p>
           <span className="font-light text-sm">Password: </span>
-          <span>******</span>
+          <span>{parsedAccount?.password}</span>
         </p>
         <Link to="/">
-          <button className="bg-black disabled:bg-black/40 text-white w-full rounded-lg py-3 mt-4 mb-2">
+          <button
+            className="bg-black disabled:bg-black/40 text-white w-full rounded-lg py-3 mt-4 mb-2"
+            disabled={!hasUserAnAccount}
+          >
             Log In
           </button>
         </Link>
@@ -31,6 +48,7 @@ function SignIn() {
         <button
           onClick={() => initializeLocalStorage()}
           className="border border-black disabled:text-black/40 disabled:border-black/40 rounded-lg mt-6 py-3"
+          disabled={hasUserAnAccount}
         >
           Sign Up
         </button>
